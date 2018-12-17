80dd3b7fd8ea427dead01a385b5a3bd248219400
theLife10 <jdgarcia9@miners.utep.edu>
2018-12-17 01:51:11 -0700

    added card and setted up tachyons for the card

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow">
+      <img alt="pot" src="https://robohash.org/test?200x200" />
+      <div>
+        <h2>jane</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
