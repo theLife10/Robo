ddff6b4f550e5b994f919863698a5d6da2c3a0b0
theLife10 <jdgarcia9@miners.utep.edu>
2018-12-18 03:15:42 -0700

    modified props and imgs

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = props => {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow">
-      <img alt="pot" src="https://robohash.org/test?200x200" />
-      <div>
-        <h2>jane</h2>
-        <p>jane.doe@gmail.com</p>
+      <img alt="pot" src={`https://robohash.org/${props.id}?200x200`} />
+      <div className="tc">
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
     </div>
   );
