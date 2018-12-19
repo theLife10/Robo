e78413c094ca502ca865ab6a8690dd547446358e
theLife10 <jdgarcia9@miners.utep.edu>
2018-12-18 23:11:57 -0700

    added cardlist

---
+++ src/CardList.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+const CardList = () => {
+  return (
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+    </div>
+  );
+};
+
+export default CardList;
--- src/index.js
+++ src/index.js
@@ -5,16 +5,9 @@ import * as serviceWorker from "./serviceWorker";
 import Card from "./Card";
 import "tachyons";
 import { robots } from "./robots";
+import CardList from "./CardList";
 
-ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<CardList robots={robots} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
