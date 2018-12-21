9811c3ab77a3e67e3222b216220c8fccd56c67d7
theLife10 <jdgarcia9@miners.utep.edu>
2018-12-20 21:42:10 -0700

    added comments

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ class App extends Component {
   onSearchChange = event => {
     this.setState({ searchfield: event.target.value });
   };
-
+  /**
+   * creating state
+   */
   render() {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name
